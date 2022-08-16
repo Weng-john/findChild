8291ac5a0c316595bf0078414d8e1f730dec9fde
Weng-john <59682713+Weng-john@users.noreply.github.com>
2022-08-17 02:15:18 +0800

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,14 +3,10 @@ var address= location.href;
 var token;
 
 address= address.split("?");
-if(address[1].length){
-    document.getElementById("main").style.display= "block";
-    document.getElementById("afterLogin").style.display= "none";
-}
-else{
+if(address.length>1){
     document.getElementById("main").style.display= "none";
     document.getElementById("afterLogin").style.display= "block";
-    address= address[1].split("=");
+    address[1].split("=");
     token= address[1];
     console.log(token);
 }
