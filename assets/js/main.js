8827de4b7224929442ccf051b2b66cf3cd69d611
Weng-john <59682713+Weng-john@users.noreply.github.com>
2022-08-17 02:36:32 +0800

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,22 @@ if(address.length>1){
     document.getElementById("afterLogin").style.display= "block";
     address= address[1].split("=");
     token= address[1];
-    console.log(token);
+    
+    $.ajax({
+        url:"https://id.nycu.edu.tw/o/token/",
+        method:"post",
+        data: {
+            grant_type: authorization_code,
+            code: token,
+            client_id: AnZOBlV6zutiSDpgG7GyTWlvQaEZ1TiXC6hNSEW8,
+            client_secret: Weg7a7ILTJON5ejEziZYZg8kDjdNmdtCEGSzWJtCPOhpG3PHPOy8twHF3PMmqrUaUazLaKyPhsO3fd9GRtly9t0VdhJnhdlc5ht73ZtenQL2dlFhDRfWFE5fvEVcAHG1,
+            redirect_uri: "https://weng-john.github.io/findChild/"
+        },
+        success: function(res){
+            console.log('ajax result:')
+            console.log(res)
+        }
+    })
 }
 
 
